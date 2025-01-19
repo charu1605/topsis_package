b9230f5f2d88524e31e27c96631495484e7346fb
charu1605 <115389902+charu1605@users.noreply.github.com>
2025-01-19 22:15:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='topsis_charu',
+ version='0.1',
+ author="charu garg",
+ author_email="gargcharu59@gmail.com",
+ description="topsis package made by charu garg",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
